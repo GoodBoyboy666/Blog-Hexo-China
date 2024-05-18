93ce6c759329448ddd6c646b5866a4d652716dfc
GoodBoyboy <me@goodboyboy.top>
2024-05-19 03:36:36 +0800

    Site updated: 2024-05-19 03:36:31

--- music/music.js
+++ music/music.js
@@ -10,57 +10,57 @@ function checkaplayer() {
                 {
                     name: "消防员康纳-主题曲(Main Theme)",
                     artist: "消防员康纳原声音轨",
-                    url: "/uploads/music/Firefighter Connor/消防员康纳-主题曲(Main Theme).mp3",
+                    url: "https://www.goodboyboy.top/uploads/music/Firefighter Connor/消防员康纳-主题曲(Main Theme).mp3",
                     artist: "APT飞果",
-                    cover: "/uploads/music/Firefighter Connor/消防员康纳原生音轨专辑封面.jpg",
+                    cover: "https://www.goodboyboy.top/uploads/music/Firefighter Connor/消防员康纳原生音轨专辑封面.jpg",
                     theme: "#c7997f"
                 },
                 {
                     name: "消防员康纳-主菜单-日间",
                     artist: "消防员康纳原声音轨",
-                    url: "/uploads/music/Firefighter Connor/主菜单-日间.mp3",
+                    url: "https://www.goodboyboy.top/uploads/music/Firefighter Connor/主菜单-日间.mp3",
                     artist: "小夏",
-                    cover: "/uploads/music/Firefighter Connor/消防员康纳原生音轨专辑封面.jpg",
+                    cover: "https://www.goodboyboy.top/uploads/music/Firefighter Connor/消防员康纳原生音轨专辑封面.jpg",
                     theme: "#c7997f"
                 },
                 {
                     name: "Ori and the Will of the Wisps-Main Theme",
                     artist: "Ori and the Will of the Wisps (Original Soundtrack)",
-                    url: "/uploads/music/Ori and the Will of the Wisps/Main Theme.mp3",
+                    url: "https://www.goodboyboy.top/uploads/music/Ori and the Will of the Wisps/Main Theme.mp3",
                     artist: "Gareth Coker",
-                    cover: "/uploads/music/Ori and the Will of the Wisps/cover.png",
+                    cover: "https://www.goodboyboy.top/uploads/music/Ori and the Will of the Wisps/cover.png",
                     theme: "#243aa2"
                 },
                 {
                     name: "Ori and the Will of the Wisps-Ku's First Flight",
                     artist: "Ori and the Will of the Wisps (Original Soundtrack)",
-                    url: "/uploads/music/Ori and the Will of the Wisps/Ku's First Flight.mp3",
+                    url: "https://www.goodboyboy.top/uploads/music/Ori and the Will of the Wisps/Ku's First Flight.mp3",
                     artist: "Gareth Coker",
-                    cover: "/uploads/music/Ori and the Will of the Wisps/cover.png",
+                    cover: "https://www.goodboyboy.top/uploads/music/Ori and the Will of the Wisps/cover.png",
                     theme: "#243aa2"
                 },
                 {
                     name: "UNDERTALE-Undertale",
                     artist: "UNDERTALE Soundtrack",
-                    url: "/uploads/music/UNDERTALE/Undertale.mp3",
+                    url: "https://www.goodboyboy.top/uploads/music/UNDERTALE/Undertale.mp3",
                     artist: "toby fox",
-                    cover: "/uploads/music/UNDERTALE/AlbumArtwork.png",
+                    cover: "https://www.goodboyboy.top/uploads/music/UNDERTALE/AlbumArtwork.png",
                     theme: "#000000"
                 },
                 {
                     name: "UNDERTALE-His Theme",
                     artist: "UNDERTALE Soundtrack",
-                    url: "/uploads/music/UNDERTALE/His Theme.mp3",
+                    url: "https://www.goodboyboy.top/uploads/music/UNDERTALE/His Theme.mp3",
                     artist: "toby fox",
-                    cover: "/uploads/music/UNDERTALE/AlbumArtwork.png",
+                    cover: "https://www.goodboyboy.top/uploads/music/UNDERTALE/AlbumArtwork.png",
                     theme: "#000000"
                 },
                 {
                     name: "Lost Ember-Come Back Home",
                     artist: "Lost Ember (Original Game Soundtrack)",
-                    url: "/uploads/music/Lost Ember/Come Back Home.mp3",
+                    url: "https://www.goodboyboy.top/uploads/music/Lost Ember/Come Back Home.mp3",
                     artist: "Will Morton, Julie Wemyss",
-                    cover: "/uploads/music/Lost Ember/Lost Ember (Original Game Soundtrack) Cover.jpg",
+                    cover: "https://www.goodboyboy.top/uploads/music/Lost Ember/Lost Ember (Original Game Soundtrack) Cover.jpg",
                     theme: "#7b2b38"
                 }
             ],
